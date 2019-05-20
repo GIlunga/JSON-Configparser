b7f1090cb5f85fe09b6cf01aa518d8ca35cdd646
Guilherme Ilunga <guilherme.ilunga@tecnico.ulisboa.pt>
2019-05-20 13:22:47 +0100

    Changed setup to allow 3.6.x

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(name="json_configparser",
-      version="0.1.0",
+      version="0.1.1",
       description="A module that parses and validates JSON configuration files.",
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setup(name="json_configparser",
                    "Programming Language :: Python :: 3.6"],
       keywords="configuration options",
       packages=["json_configparser"],
-      python_requires="==3.6",
+      python_requires=">=3.6 <3.7",
       install_requires=[],
       extras_require={"test": ["pytest", "pytest-cov", "pytest-remotedata"]},
       project_urls={"Bug Reports": "https://github.com/GIlunga/JSON-Configparser/issues",
